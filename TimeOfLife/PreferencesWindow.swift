ea6c4ee1af3ba180e5df22d0660a0b4c2fcf5f0b
jeremaihloo <1006397539@qq.com>
2018-02-08 10:26:46 +0800

    first commit

---
+++ TimeOfLife/PreferencesWindow.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesWindow.swift
+//  TimeOfLife
+//
+//  Created by jj on 2018/2/8.
+//  Copyright © 2018年 jeremaihloo. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
---
+++ TimeOfLife/StatusMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  StatusMenuController.swift
+//  TimeOfLife
+//
+//  Created by jj on 2018/2/8.
+//  Copyright © 2018年 jeremaihloo. All rights reserved.
+//
+
+import Foundation
